9773cc602990730377ec6a7d76b3310414e7557a
qehrhino <qehrhino@qehrhino>
2016-08-10 14:57:06 +0930

    implemented KeepSamples and ExcludeSamples transforms

--- omicexperiment/transforms/sample.py
+++ omicexperiment/transforms/sample.py
@@ -1,6 +1,33 @@
 import numpy as np
 from omicexperiment.transforms.transform import Transform
 
+
+class KeepSamples(Transform):
+    def __init__(self, sample_names):
+        self.sample_names = list(sample_names)
+    
+    def __dapply__(self, experiment):
+        to_keep = experiment.data_df.columns.intersection(self.sample_names)
+        return experiment.data_df.reindex(columns=to_keep)
+
+    def __eapply__(self, experiment):
+        kept_samples_df = self.__dapply__(experiment)
+        return experiment.with_data_df(kept_samples_df)
+
+
+class ExcludeSamples(Transform):
+    def __init__(self, sample_names, errors='ignore'):
+        self.sample_names = list(sample_names)
+        self.errors = errors
+    
+    def __dapply__(self, experiment):
+        return experiment.data_df.drop(self.sample_names, axis=1, errors=self.errors)
+
+    def __eapply__(self, experiment):
+        after_exc = self.__dapply__(experiment)
+        return experiment.with_data_df(after_exc)
+
+
 class SampleGroupBy(Transform):
     def __init__(self, variable, aggfunc=np.mean):
         self.variable = variable
